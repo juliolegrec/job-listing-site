3db9d688bed8a0def7d649f57c804c3b74ac6ecd
Julio Legrec <jlegrec@proximitybbdo.fr>
2021-11-23 21:28:17 +0400

    filter bar

--- app/components/listing.js
+++ app/components/listing.js
@@ -1,10 +1,38 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ListingComponent extends Component {
-  @tracked model = this.args.model;
+  @tracked listings = this.args.model;
 
-  testModel() {
-    console.log(model);
+  @tracked filters = {
+    role: '',
+    level: '',
+    language: '',
+    tool: '',
+  };
+
+  get filterListings() {
+    const filteredListings = this.listings;
+
+    if (this.role) {
+      return filteredListings.filter((listing) => listing.role === this.role);
+    } else {
+      return filteredListings;
+    }
+  }
+
+  @action
+  changeFilter(event) {
+    let filterType = event.srcElement.dataset.type;
+    let filterParam = event.srcElement.innerText;
+
+    this.filters[filterType] = filterParam;
+    console.log(this.filters);
+  }
+
+  @action
+  clearFilter() {
+    this.role = '';
   }
 }
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,4 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 
-export default class IndexController extends Controller {
-  queryParams = ['language'];
-
-  @tracked language = null;
-
-  @tracked model;
-
-  get filtedLanguages() {
-    let language = this.language;
-    let languages = this.model.languages;
-
-    console.log(language);
-    console.log(languages);
-  }
-}
+export default class IndexController extends Controller {}
