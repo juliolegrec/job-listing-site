26aa7284705a057e98e60d17ae2d56b6b59577ce
Julio Legrec <julio.legrec@gmail.com>
2021-12-02 21:09:31 +0400

    Filtering...

--- app/components/listing.js
+++ app/components/listing.js
@@ -3,43 +3,68 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ListingComponent extends Component {
+  @tracked isFiltered = false;
   @tracked listings = this.args.model;
-
   @tracked filters = {
-    role: ['Frontend', 'Fullstack', 'Backend'],
-    level: ['Senior', 'Junior', 'Midweight'],
+    role: [],
+    level: [],
+    language: [],
+    tool: [],
   };
+  @tracked filterTags = [];
 
-  get filterListings() {
-    const filteredListings = this.listings.filter((listing) => {
-      // this.filters.role.forEach((el) => console.log(el));
-      // console.log(listing.role);
-      this.filters.role.forEach((el) => el === listing.role);
-    });
-    return filteredListings;
+  get filteredListings() {
+    if (this.isFiltered) {
+      // filter the listings based on filters
+
+      return this.listings.filter((listing) => {
+        for (let filter of this.filters.role) {
+          return listing.role === filter;
+        }
+      });
+    } else {
+      return this.listings;
+    }
   }
 
-  @action
-  changeFilter(event) {
+  changeFilter = (event) => {
+    this.isFiltered = true;
+
     let filterType = event.srcElement.dataset.type;
     let filterValue = event.srcElement.innerText;
 
-    // if (this.filters[filterType].find((el) => el === filterValue)) {
-    //   return;
-    // }
-    // this.filters[filterType].push(filterValue);
+    if (this.filters[filterType].find((el) => el === filterValue)) {
+      return;
+    } else {
+      this.filters[filterType].push(filterValue);
+    }
+  };
 
-    this.filters[filterType] = filterValue;
-    console.table(this.filters);
+  get filterTags() {
+    for (const [key, value] of Object.entries(this.filters)) {
+      if (value.length === 0) {
+        return;
+      } else {
+        this.filterTags.push(value);
+      }
+      // console.log(`
+      // Key: ${key}
+      // Value: ${value}
+      // `);
+    }
+
+    console.log(this.filterTags);
+
+    return this.filterTags;
   }
 
-  @action
-  clearFilter() {
+  clearFilter = () => {
+    this.isFiltered = false;
     this.filters = {
       role: [],
       level: [],
       language: [],
       tool: [],
     };
-  }
+  };
 }
